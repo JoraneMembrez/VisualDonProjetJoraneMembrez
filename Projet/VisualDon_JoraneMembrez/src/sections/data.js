c93af73de2537d0b09c3c2a8498f794961c69e5f
Jorane Membrez <jorane.membrez@outlook.com>
2023-05-14 17:14:56 +0200

    test await

--- Projet/VisualDon_JoraneMembrez/src/sections/data.js
+++ Projet/VisualDon_JoraneMembrez/src/sections/data.js
@@ -1,11 +1,10 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 //import dataBrut from "../data/anamia.csv";
 
-const dataBrut = d3.csv("../data/anamia.csv");
+const dataBrut = await d3.csv("../data/anamia.csv");
 //import { nest } from "https://cdn.jsdelivr.net/npm/d3-collection@1.0.7/dist/d3-collection.min.js";
 
 const rendToutesDonnes = () => {
-  console.log(dataBrut);
   return dataBrut;
 };
 
